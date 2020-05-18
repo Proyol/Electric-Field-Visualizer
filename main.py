ab5727360fe73041753675ba2e727c7a13506e6c
Proyol <52104910+Proyol@users.noreply.github.com>
2020-05-18 14:52:09 -0300

    Gerar grade
    Adicionado a função de gerar uma grade no canvas a partir de input do usuário

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-import _tkinter as tk
+import tkinter as tk
 
 from tkinter import *
 from PIL import Image
 from pynput.mouse import Button, Controller, Listener
 
-root = Tk()
-mouse = Controller()
-
 cw = 600
 ch = 600
 
+root =Tk()
+root.title("Electric field visualizer")
+root.maxsize(cw, ch)
+mouse = Controller()
+
 # adicionar um canvas na tela
 canvas = Canvas(root, height=ch, width=cw, bg="black")
-canvas.pack()
+canvas.grid(column=0, row=0)
 
 # armazenar as cargas em um vetor
 pos=[]
 neg=[]
 
-# desenhar os vetores
-def draw_vector():
-    for i in range(int(ch / 60)):
-        for j in range(int(cw / 60)):
-            canvas.create_line(j*60, i*60, j*60 + 40, i*60 + 40, fill="white", arrow="last", width=3)
+
+# desenhar grid
+def draw_grid():
+    scl = int(cw/size.get())
+
+    i = 0
+    canvas.delete("all")
+    while i < cw:
+        canvas.create_line(0, i, cw, i, fill="gray", width=1)  # linhas horizontais
+        canvas.create_line(i, 0, i, ch, fill="gray", width=1)  # linhas verticais
+        i += scl
+
+
+# determinar o tamanho da grade do canvas
+size = tk.IntVar()
+size_box = tk.Entry(root, width=30, textvariable=size)
+size_box.place(x=10, y=10)
+
+submit = tk.Button(root, text="Enviar", width=10, command=draw_grid)
+submit.place(x=10, y=40)
+
 
 # adicionar as cargas na tela
 def insert_pos(event):
@@ -34,6 +52,7 @@ def insert_pos(event):
         canvas.create_image(x, y, image=img)
         canvas.image = img
 
+
 def insert_neg(event):
     x, y = event.x, event.y
 
@@ -43,10 +62,9 @@ def insert_neg(event):
         canvas.create_image(x, y, image=img)
         canvas.image = img
 
+
 # funções para o mouse
 root.bind('<Button-1>', insert_pos)
 root.bind('<Button-3>', insert_neg)
-
-draw_vector()
 
 mainloop()
